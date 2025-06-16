5dd5b418e6077db62be5bb3f824befeaf0ec27d4
shankhadeepk <shankhadeep.it@gmail.com>
2025-06-17 00:40:11 +0100

    Important code base

--- src/main/java/com/leetcodeproj/leetcode/DistributeCandiesPeople.java
+++ src/main/java/com/leetcodeproj/leetcode/DistributeCandiesPeople.java
@@ -7,7 +7,7 @@ package com.leetcodeproj.leetcode;
 * */
 public class DistributeCandiesPeople {
 
-    public int[] distributeCandies(int candies, int num_people) {
+    /*public int[] distributeCandies(int candies, int num_people) {
         int [] result = new int[num_people];
         int count=1;
         int i=0;
@@ -27,14 +27,31 @@ public class DistributeCandiesPeople {
             result[i]=result[i]+candies;
         }
         return result;
-    }
+    }*/
 
     public static void main(String[] args) {
         DistributeCandiesPeople distributeCandiesPeople=new DistributeCandiesPeople();
-        int[] result=distributeCandiesPeople.distributeCandies(60,4);
+        int[] result=distributeCandiesPeople.distributeCandies(10,3);
 
         for (int i=0;i<result.length;i++){
             System.out.print(" "+result[i]);
         }
+    }
+
+    public int[] distributeCandies(int candies, int num_people){
+        int[] result = new int[num_people];
+        int count = candies;
+        for(int i=1,j=0;i<=candies;i++,j++){
+            if (j == num_people){
+                j=0;
+            }
+            if(i > count){
+                result[j]=result[j] + count;break;
+            }else {
+                result[j]=result[j]+i;
+                count=count-i;
+            }
+        }
+        return result;
     }
 }
